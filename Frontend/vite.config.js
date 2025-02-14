69518758cfdd794b40ef8d3be504366bef157d8d
om74202 <ommishra74202@gmail.com>
2025-02-14 10:41:10 +0530

    BugFix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // Allows access from any IP
+    port: 5173,        // Change to your desired port
+  },
   plugins: [
     tailwindcss(),
     react()],
